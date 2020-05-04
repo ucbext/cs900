694153f695656a240e7664327042458f3ffa9b60
Faraaz Sareshwala <faraazs@gmail.com>
2020-05-03 18:47:53 -0700

    program5: add dynamic array tests

--- program5/test_dynamic_array.cc
+++ program5/test_dynamic_array.cc
@@ -1,3 +1,20 @@
 #include <gtest/gtest.h>
 
-TEST(DynamicArrayTest, Foo) {}
+#include "program5/dynamic_array.h"
+
+TEST(DynamicArrayTest, DefaultConstructor) {
+  DynamicArray<int> array;
+  ASSERT_EQ(0, array.get_size());
+  ASSERT_EQ(0, array.get_capacity());
+}
+
+TEST(DynamicArrayTest, CapacityConstructor) {
+  size_t size = 10;
+  DynamicArray<int> array(size);
+  ASSERT_EQ(size, array.get_size());
+  ASSERT_EQ(size, array.get_capacity());
+}
+
+TEST(DynamicArrayTest, At) {
+
+}

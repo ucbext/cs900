750c995aa7f61410d3117d3d0251e811041481d8
Faraaz Sareshwala <faraazs@gmail.com>
2020-06-05 00:28:37 -0700

    tools: fix repository location bug

--- tools/update.sh
+++ tools/update.sh
@@ -4,7 +4,7 @@
 
 function main() {
     git remote remove upstream
-    git remote add upstream git@github.com:fsareshwala/cs404.1.git
+    git remote add upstream git@github.com:fsareshwala/cs900.git
     git fetch upstream
     git merge upstream/master
 }
